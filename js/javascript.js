5ad00b0ebccddf824ff7c93bc0cff445cc12b450
Viteck <99819977+Viteck@users.noreply.github.com>
2023-11-20 14:34:07 +0000

    Delete js directory

--- js/javascript.js
+++
@@ -1,4 +0,0 @@
-function showGallery(){
-	var gallery = document.getElementById("modal-print").style.display = "initial";
-}
-var galleryLink = document.getElementById("foto1").onclick = showGallery;
